95b6ffc3acaebc0573ad0a685d751b32d01c709c
rudjuh <rud_olf@hotmail.com>
2015-05-28 21:55:33 +0100

    event-view page

---
+++ event_view.php
@@ -0,0 +1,81 @@
+<?php
+include 'header.php';
+?>
+<div class="row content-container">
+	<div class="row content">
+		<div class="col-md-3">
+			<img class="event-img-view" src="http://www.guestlist.net/uploads/events/2015/05/16902.jpg" width="100%"/>
+			<div class="box-prices">
+				<div class="ticket-button guestlist">
+					<span>GUESTLIST</span>
+					<span>&pound;50</span>
+				</div>
+				<div class="ticket-button normal">
+					<span>NORMAL TICKET</span>
+					<span>BUY</span>
+				</div>
+				<div class="ticket-button other">
+					<span>DOOR TICKETS</span>
+					<span>&pound;15</span>
+				</div>
+				<div class="ticket-button other">
+					<span>VIP TICKETS</span>
+					<span>&pound;15</span>
+				</div>
+			</div>
+			<span class="attending">PEOPLE ATTENDING</span>
+			<span class="showhide attend-hide sel">HIDE</span>
+			<span class="showhide">&nbsp;\&nbsp;</span>
+			<span class="showhide attend-show">SHOW</span>
+			<div style="display: none;" class="attending-guestlist">
+				<ul>
+						<li><span><a href="/profile/">hallo</a></span></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="event-view">
+				<h1>Jungle Splash</h1>
+				<div class="event-info">
+					<div>
+						<span class="calender"></span>
+						<h3>Saturday 4th Jul 2015</h3>
+					</div>
+					<div>
+						<span class="clock"></span>
+						<h3>10pm - 6am</h3>
+					</div>
+					<div>
+						<span class="genre"></span>
+						<h3>Drum and Bass</h3>
+					</div>
+					<div>
+						<span class="location"></span>
+						<h3>The Bussey Building, 133 Rye Lane, SE15 4ST</h3>
+					</div>
+					<br class="clr" />
+				</div>
+				
+				<div class="event-content">
+					<span>LINE UP</span>
+					<p>Siobhan Bell, Daktyl & The Ninetys</p>
+					<br>
+					<span>INFO</span>
+					<p>London’s newest, monthly live music show Sound Fidelity launches next Thursday night at the jewel in Peckham’s crown, The Bussey Building. Hosted by Peigh of Peace Love Unity featuring music from London's finest, emerging Soul, Bass, Dubstep & Trap acts, Sound Fidelity introduces a refreshing fusion of urban music and contemporary circus arts. Kicking off at 10pm, the night will be laced with bass, heavy, beats (DJs include Daktyl and Siobhan Bell) and lit with ultra violet lights. Electro RnB duo Pandr Eyez make their South London debut supported by soul songstresses Catherine-Sera & Asia Love Musiq and also Trap siren Shamz Le Roc. Also on the night you can expect living statues, glow-in-the-dark body poppers, resident photographers and more.</p>
+				</div>
+
+			</div>
+			
+		</div>
+		<div class="col-md-3">
+			<div class="more-events">
+				<span>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<?php
+include 'footer.php';
+?>
