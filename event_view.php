778a5b427a4d461bfba0e0f34359eec1ea9ecfd3
rudjuh <rud_olf@hotmail.com>
2015-05-29 15:21:24 +0100

    event view almost done

--- event_view.php
+++ event_view.php
@@ -3,7 +3,7 @@ include 'header.php';
 ?>
 <div class="row content-container">
 	<div class="row content">
-		<div class="col-md-3">
+		<div class="col-md-3 col-sm-5">
 			<img class="event-img-view" src="http://www.guestlist.net/uploads/events/2015/05/16902.jpg" width="100%"/>
 			<div class="box-prices">
 				<div class="ticket-button guestlist">
@@ -31,9 +31,36 @@ include 'header.php';
 				<ul>
 						<li><span><a href="/profile/">hallo</a></span></li>
 				</ul>
+			</div><br /><br />
+			<div class="venue-events visible-sm">
+				<span class="venue-events-title">OTHER EVENTS ON THIS VENUE</span>
+				<div class="venue-events-lmt">
+					<div class="venue-event-img">
+						<img src="http://3.static.newspaper.guestlistmedia.net/uploads/events/9/858_medium.jpg" width="100%"/>
+					</div>
+					<h1>GAZ'S ROCKIN..</h1>
+					<span>Wednesday 30,</span><span style="color: #773d8e; font-weight: 600;"> -Nov -0001</span>
+					<h2>The Alibi</h2>
+				</div>
+				<div class="venue-events-lmt">
+					<div class="venue-event-img">
+						<img src="http://3.static.newspaper.guestlistmedia.net/uploads/events/9/858_medium.jpg" width="100%"/>
+					</div>
+					<h1>GAZ'S ROCKIN..</h1>
+					<span>Wednesday 30,</span><span style="color: #773d8e; font-weight: 600;"> -Nov -0001</span>
+					<h2>The Alibi</h2>
+				</div>
+				<div class="venue-events-lmt">
+					<div class="venue-event-img">
+						<img src="http://3.static.newspaper.guestlistmedia.net/uploads/events/9/858_medium.jpg" width="100%"/>
+					</div>
+					<h1>GAZ'S ROCKIN..</h1>
+					<span>Wednesday 30,</span><span style="color: #773d8e; font-weight: 600;"> -Nov -0001</span>
+					<h2>The Alibi</h2>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-5">
+		<div class="col-md-5 col-sm-7">
 			<div class="event-view">
 				<h1>Jungle Splash</h1>
 				<div class="event-info">
@@ -62,24 +89,43 @@ include 'header.php';
 					<br>
 					<span>INFO</span>
 					<p>London�s newest, monthly live music show Sound Fidelity launches next Thursday night at the jewel in Peckham�s crown, The Bussey Building. Hosted by Peigh of Peace Love Unity featuring music from London's finest, emerging Soul, Bass, Dubstep & Trap acts, Sound Fidelity introduces a refreshing fusion of urban music and contemporary circus arts. Kicking off at 10pm, the night will be laced with bass, heavy, beats (DJs include Daktyl and Siobhan Bell) and lit with ultra violet lights. Electro RnB duo Pandr Eyez make their South London debut supported by soul songstresses Catherine-Sera & Asia Love Musiq and also Trap siren Shamz Le Roc. Also on the night you can expect living statues, glow-in-the-dark body poppers, resident photographers and more.</p>
+					<p>London�s newest, monthly live music show Sound Fidelity launches next Thursday night at the jewel in Peckham�s crown</p>
 				</div>
 
 			</div>
 			
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-4 col-sm-5 hidden-sm">
 			<div class="venue-events">
-				<span>OTHER EVENTS ON THIS VENUE</span>
+				<span class="venue-events-title">OTHER EVENTS ON THIS VENUE</span>
 				<div class="venue-events-lmt">
 					<div class="venue-event-img">
 						<img src="http://3.static.newspaper.guestlistmedia.net/uploads/events/9/858_medium.jpg" width="100%"/>
 					</div>
 					<h1>GAZ'S ROCKIN..</h1>
-					<span>Wednesday 30,  Nov -0001</span>
+					<span>Wednesday 30,</span><span style="color: #773d8e; font-weight: 600;"> -Nov -0001</span>
+					<h2>The Alibi</h2>
+				</div>
+				<div class="venue-events-lmt">
+					<div class="venue-event-img">
+						<img src="http://3.static.newspaper.guestlistmedia.net/uploads/events/9/858_medium.jpg" width="100%"/>
+					</div>
+					<h1>GAZ'S ROCKIN..</h1>
+					<span>Wednesday 30,</span><span style="color: #773d8e; font-weight: 600;"> -Nov -0001</span>
+					<h2>The Alibi</h2>
+				</div>
+				<div class="venue-events-lmt">
+					<div class="venue-event-img">
+						<img src="http://3.static.newspaper.guestlistmedia.net/uploads/events/9/858_medium.jpg" width="100%"/>
+					</div>
+					<h1>GAZ'S ROCKIN..</h1>
+					<span>Wednesday 30,</span><span style="color: #773d8e; font-weight: 600;"> -Nov -0001</span>
 					<h2>The Alibi</h2>
 				</div>
 			</div>
 		</div>
+
+
 	</div>
 </div>
 <div id="booked-success" class="modal fade bs-no-stripe" tabindex="-1" role="dialog" aria-labelledby="noStripeLabel" aria-hidden="true">
